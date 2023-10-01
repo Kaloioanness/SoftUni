8d834aa61bae5baa1a3e1370fdcf951615c542c7
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-10-01 09:22:21 +0300

    Add files via upload

---
+++ Java Fundamentals/Lab/BasicSyntaxConditionalStatementsAndLoops/P02Passed.java
@@ -0,0 +1,13 @@
+package JavaFundamentals.Lab.BasicSyntaxConditionalStatementsAndLoops;
+import java.util.Scanner;
+
+public class P02Passed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+        if (grade >= 3){
+            System.out.println("Passed!");
+        }
+
+    }
+}
