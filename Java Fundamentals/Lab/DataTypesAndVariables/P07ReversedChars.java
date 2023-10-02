c7ed4a212bb912328604f2ab2d3130c96ac13b90
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-10-02 09:08:12 +0300

    Add files via upload

---
+++ Java Fundamentals/Lab/DataTypesAndVariables/P07ReversedChars.java
@@ -0,0 +1,14 @@
+package SoftUni.DataTypesAndVariables;
+import java.util.Scanner;
+
+public class P07ReversedChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char firstSymbol = scanner.nextLine().charAt(0);
+        char secondSymbol = scanner.nextLine().charAt(0);
+        char thirdSymbol = scanner.nextLine().charAt(0);
+
+        System.out.printf("%c %c %c", thirdSymbol,secondSymbol,firstSymbol);
+    }
+}
