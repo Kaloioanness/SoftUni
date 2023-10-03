83ece63d37a3a323dca8bb363fa384bb3173be20
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-10-03 08:49:20 +0300

    Add files via upload

---
+++ Java Fundamentals/Lab/Arrays/P07CondenseArrayToNumber.java
@@ -0,0 +1,21 @@
+package SoftUni.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P07CondenseArrayToNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        while (numbers.length > 1) {
+            int[] condensed = new int[numbers.length - 1];
+            for (int i = 0; i < numbers.length - 1; i++) {
+                condensed[i] = numbers[i] + numbers[i + 1];
+            }
+            numbers = condensed;
+        }
+        System.out.println(numbers[0]);
+    }
+}
