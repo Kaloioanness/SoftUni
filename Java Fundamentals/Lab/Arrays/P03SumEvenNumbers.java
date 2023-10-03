554be71275492ab989a6768284aa49d3269b6d5f
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-10-03 08:48:06 +0300

    Add files via upload

---
+++ Java Fundamentals/Lab/Arrays/P03SumEvenNumbers.java
@@ -0,0 +1,22 @@
+package SoftUni.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P03SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int current = numbers[i];
+            if (current % 2 ==0){
+                sum += current;
+            }
+        }
+        System.out.println(sum);
+    }
+
+}
