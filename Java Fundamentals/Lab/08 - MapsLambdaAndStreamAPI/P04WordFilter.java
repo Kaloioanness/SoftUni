0e3ec0e1e767646b3d4832d414a654da8a31be43
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-10-07 09:59:37 +0300

    Add files via upload

---
+++ Java Fundamentals/Lab/08 - MapsLambdaAndStreamAPI/P04WordFilter.java
@@ -0,0 +1,16 @@
+package SoftUni.MapsLambdaAndStreamAPI;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P04WordFilter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = Arrays.stream(scanner.nextLine().split(" ")).filter(w -> w.length() % 2 == 0).toArray(String[]::new);
+        for (String word :
+                words) {
+            System.out.println(word);
+        }
+
+    }
+}
