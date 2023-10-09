ce0156cf71b18a38d39cf3b18d136fcc7c8ba841
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-10-09 08:51:47 +0300

    Add files via upload

---
+++ Java Fundamentals/Lab/10 - RegularExpressions/P02MatchPhoneNumber.java
@@ -0,0 +1,21 @@
+package SoftUni.RegularExpressions;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P02MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String regex = "\\+359([ |-])[2]\\1\\d{3}\\1\\d{4}\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        List<String> matched = new ArrayList<>();
+        while (matcher.find()){
+            matched.add(matcher.group());
+        }
+        System.out.print(String.join(", ",matched));
+    }
+}
