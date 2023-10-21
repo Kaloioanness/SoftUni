18a092f8d1f267280c5445fad96fefe35b82d2d9
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-10-21 21:26:16 +0300

    Add files via upload

---
+++ JavaAdvanced/Excercises/03 - SetsAndMapsAdvanced/P10LogsAggregator.java
@@ -0,0 +1,36 @@
+package JavaAdvanced.Excercises.SetsAndMapsAdvanced;
+
+import java.util.*;
+
+public class P10LogsAggregator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        TreeMap<String, Integer> usersWithTime = new TreeMap<>();
+        TreeMap<String, TreeSet<String>> addresses = new TreeMap<>();
+
+        while (n-- > 0) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+
+            String ip = tokens[0];
+            String name = tokens[1];
+            int time = Integer.parseInt(tokens[2]);
+
+            if(!usersWithTime.containsKey(name)){
+                usersWithTime.put(name, time);
+                addresses.put(name, new TreeSet<>(){{add(ip);}});
+            }else{
+                usersWithTime.put(name, usersWithTime.get(name)  + time);
+                addresses.get(name).add(ip);
+            }
+        }
+
+        usersWithTime.forEach((key, value) ->{
+
+            System.out.println(String.format("%s: %d %s", key, value, addresses.get(key).toString()));
+
+        });
+    }
+}
