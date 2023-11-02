8de2755e8085927854378da77e3e2e7d219f21da
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-11-02 19:58:45 +0200

    Add files via upload

---
+++ JavaAdvanced/Excercises/06 - Difine Classes/P04RawData/Cargo.java
@@ -0,0 +1,12 @@
+package JavaAdvanced.Excercises.DefiningClasses.P04RawData;
+
+public class Cargo {
+    //1000 fragile
+    int cargoWeight;
+    String cargoType;
+
+    public Cargo(int cargoWeight, String cargoType) {
+        this.cargoWeight = cargoWeight;
+        this.cargoType = cargoType;
+    }
+}
