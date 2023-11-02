9338559dd3f2549c5f77b6b3716f1ab85ddbd423
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2023-11-02 19:59:40 +0200

    Add files via upload

---
+++ JavaAdvanced/Excercises/06 - Difine Classes/P04RawData/Engine.java
@@ -0,0 +1,15 @@
+package JavaAdvanced.Excercises.DefiningClasses.P04RawData;
+
+public class Engine {
+    int engineSpeed;
+    int enginePower;
+
+    public Engine(int engineSpeed, int enginePower) {
+        this.engineSpeed = engineSpeed;
+        this.enginePower = enginePower;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+}
