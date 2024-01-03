9a9c842bd4962648538a49915c9e7eb05b43b3e0
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2024-01-03 19:38:05 +0200

    Add files via upload

---
+++ JavaAdvanced/Excercises/06 - Difine Classes/P07Google/Car.java
@@ -0,0 +1,18 @@
+package JavaAdvanced.Excercises.DefiningClasses.P07Google;
+
+public class Car {
+    //характеристики
+    private String carModel;
+    private int carSpeed;
+
+
+    public Car(String carModel, int carSpeed) {
+        this.carModel = carModel;
+        this.carSpeed = carSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return carModel + " " + carSpeed;
+    }
+}
