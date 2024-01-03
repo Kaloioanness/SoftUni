2278aea40fea8364428ae1161c1fbad96dace31d
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2024-01-03 19:38:39 +0200

    Add files via upload

---
+++ JavaAdvanced/Excercises/06 - Difine Classes/P07Google/Child.java
@@ -0,0 +1,16 @@
+package JavaAdvanced.Excercises.DefiningClasses.P07Google;
+
+public class Child {
+    private String childName;
+    private String childBirthday;
+
+    public Child(String childName, String childBirthday) {
+        this.childName = childName;
+        this.childBirthday = childBirthday;
+    }
+
+    @Override
+    public String toString() {
+        return childName + " " + childBirthday;
+    }
+}
